44a35ca6a31f275057c2fba31c9d6e6a16845632
girigiribauer <girigiribauer@gmail.com>
2023-10-24 08:03:01 +0900

    最低限のガワ修正

---
+++ dev.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+hugo server --buildDrafts --buildFuture --port 2424
+open http://localhost:2424
--- localserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-open http://localhost:2424 && hugo server --buildDrafts --buildFuture --port 2424
